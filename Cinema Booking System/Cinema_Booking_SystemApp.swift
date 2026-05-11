f66a871c99103993f120428cfeb59f1895d2f0b5
Damaq Zain <zaindamaq@gmail.com>
2026-05-11 18:09:00 +1000

    Initial Commit

---
+++ Cinema Booking System/Cinema_Booking_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema_Booking_SystemApp.swift
+//  Cinema Booking System
+//
+//  Created by Damaq Mohd Zain on 11/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Cinema_Booking_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinema Booking System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinema Booking System
+//
+//  Created by Damaq Mohd Zain on 11/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
